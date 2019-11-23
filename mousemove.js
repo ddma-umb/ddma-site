183523be21b440a62dea12b21897869fd6469848
Remy Hunter <remyhunter256@gmail.com>
2019-11-23 17:22:16 -0500

    add some code for floating image lement

---
+++ mousemove.js
@@ -0,0 +1,15 @@
+console.log('got here');
+
+var el = ".floater"
+
+
+$(document).ready(function(){
+	$(window).mousemove(function(evt){
+
+	    $("el").css({
+	        "left": -(evt.clientX * 0.05),
+	        "top": -(evt.clientY * 0.05),
+	    });
+
+	});
+});
